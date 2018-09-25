fe01c1ff55bb89130238fe96189a5251bbd6893a
talktobrent <talktobrent@gmail.com>
2018-09-25 16:43:23 -0700

    Created function

---
+++ builtins.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+
+int builtins(array)
+{
+	instruction_t builtins[] = {
+		{"push", push},
+		{"pall", pall},
+		{"pint", pint},
+		{"pop", pop},
+		{"swap", swap},
+		{"add", add},
+		{"nop", nop},
+		{NULL, NULL},
+	};
+
+	for (count = 0; builtins[count].opcode != NULL; count++)
+	{
+		if ((strcmp(array[0])) == 0)
+			return ((builtins[count].f(array[1])));
+	}
+	return ();
+}
