37500b358573c2e08724a5ea23153a9becfcf229
VortexH <hemant.heer@gmail.com>
2018-09-26 02:39:46 +0000

    populated header with prototypes and headers

---
+++ monty.h
@@ -0,0 +1,49 @@
+#ifndef _MONTY_H
+#define _MONTY_H
+
+#include <ctype.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct stack_s
+{
+        int n;
+        struct stack_s *prev;
+        struct stack_s *next;
+} stack_t;
+
+stack_t *head = NULL;
+
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct instruction_s
+{
+        char *opcode;
+        void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+void buildarray(char *buffer, char **array);
+int push(char *string);
+int pall(char *string);
+void errprint(int error, int line, char *filename, char *oparg);
+int builtins(char **array);
+
+#endif
