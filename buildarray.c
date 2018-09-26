0275f90a92fa79396d3df15549a132ba7dc0937d
VortexH <hemant.heer@gmail.com>
2018-09-26 00:52:10 +0000

    initial commit

---
+++ buildarray.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+
+/**
+ * buildarray - generates tokens from buffer and stores tokens in an array
+ * @buffer: buffer allocated from getline and passed into func to get toks
+ * @array: address of the char *array declared in main
+ * Description: repeated calls to strtok are made as long as incrementer is
+ * less than 2 and token is not null
+ * Return: Does not return anything
+*/
+
+void buildarray(char *buffer, char **array)
+{
+	char *token;
+	int i;
+
+	token = strtok(buffer, " \n");
+	for (i = 0; token != NULL && i < 2; i++)
+	{
+		*array[i] = token;
+		token = strtok(NULL, " \n");
+	}
+}
