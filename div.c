20a1fdc4e934655ba049a63d023aa98e056feae3
VortexH <hemant.heer@gmail.com>
2018-09-27 19:39:40 +0000

    div opcode functionality

---
+++ div.c
@@ -0,0 +1,33 @@
+#include "monty.h"
+
+/**
+ * divop - divides the 2ndtop element of the stack by the top element of the
+ * stack
+ *
+ * @stack: doubly linked list representation of the stack
+ * @line_number: the line that opcode exists at
+ *
+ * Description: Throws an error if the stack contains less than 2 elements or
+ * if the top element of the stack is 0.
+ *
+ * Return: void
+*/
+
+void divop(stack_t **stack, unsigned int line_number)
+{
+	if (stacklength(*stack) < 2)
+	{
+		all.errorcode = 14;
+		return;
+	}
+
+	if ((*stack)->n == 0)
+	{
+		all.errorcode = 15;
+		return;
+	}
+
+	(*stack)->prev->n = (*stack)->prev->n / (*stack)->n;
+	pop(stack, line_number);
+
+}
