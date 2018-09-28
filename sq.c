fae6572172ac6ab9bb72c90016c1bf02e918d1c2
talktobrent <talktobrent@gmail.com>
2018-09-27 21:37:21 -0700

    Fixed stack and queue functionality to not reverse whole list upon mode change

--- sq.c
+++ sq.c
@@ -1,33 +1,19 @@
 #include "monty.h"
 
 /**
- * sq - reverses whole stack if "stack" entered
+ * sq - switches stack on
  * @stack: top of stack
  * @line_number: current line of monty file
- * Description: reverses the order of the stack (moves top to bottom)
  */
 
 void sq(stack_t **stack, unsigned int line_number)
 {
-	stack_t *current;
 	(void)(line_number);
+	(void)(stack);
 
 	if (all.stack == 1)
 		return;
 
-	all.stack = 0;
+	all.stack = 1;
 
-	if (*stack != NULL)
-	{
-		current = *stack;
-		while (current->prev != NULL)
-		{
-			current = current->prev;
-			current->next->prev = current->next->next;
-			current->next->next = current;
-		}
-		current->prev = current->next;
-		current->next = NULL;
-		*stack = current;
-	}
 }
--- sq0.c
+++ sq0.c
@@ -1,33 +1,19 @@
 #include "monty.h"
 
 /**
- * sq0 - reverses whole stack if "queue" entered
+ * sq0 - switches stack off
  * @stack: top of stack
  * @line_number: current line of monty file
- * Description: reverses the order of the stack (moves top to bottom)
  */
 
 void sq0(stack_t **stack, unsigned int line_number)
 {
-	stack_t *current;
 	(void)(line_number);
+	(void)(stack);
 
 	if (all.stack == 0)
 		return;
 
-	all.stack = 1;
+	all.stack = 0;
 
-	if (*stack != NULL)
-	{
-		current = *stack;
-		while (current->prev != NULL)
-		{
-			current = current->prev;
-			current->next->prev = current->next->next;
-			current->next->next = current;
-		}
-		current->prev = current->next;
-		current->next = NULL;
-		*stack = current;
-	}
 }
