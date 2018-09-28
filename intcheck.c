9538e30e727afe6a17b3428aee4612b2365128de
talktobrent <talktobrent@gmail.com>
2018-09-27 18:39:49 -0700

    Added condition to check for a string of just '-'

--- intcheck.c
+++ intcheck.c
@@ -16,7 +16,7 @@ int intcheck(void)
 		all.errorcode = 5;
 		return (-1);
 	}
-	if (all.arr[1][0] == '-')
+	if (all.arr[1][0] == '-' && all.arr[1][1] != '\0')
 		count = 1;
 
 	while (isdigit(all.arr[1][count]) > 0)
